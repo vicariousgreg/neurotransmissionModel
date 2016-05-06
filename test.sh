5985bdb21feef9db328e7d970672f0be9b962941
Gregory Davis <vicariousgreg@github.com>
2016-05-06 00:09:48 -0700

    made test script loop

--- test.sh
+++ test.sh
@@ -1,5 +1,3 @@
-python test_axon.py -v -s
-python test_dendrite.py -v -s
-python test_depression.py -v -s
-python test_sustained.py -v -s
-python test_synaptic_cleft.py -v -s
+for t in $( ls test*.py ); do
+  python $t -s -v
+done
