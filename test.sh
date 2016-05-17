d2a10a87b3168f3fd63cb65330e4fd1184ca54e2
Gregory Davis <vicariousgreg@github.com>
2016-05-16 22:05:24 -0700

    added verbose argument to test.sh (lazily)

--- test.sh
+++ test.sh
@@ -1,4 +1,12 @@
-for t in $( ls test*.py ); do
-  echo $t
-  python $t -s
-done
+if [ ! -z $1 ] 
+then 
+    for t in $( ls test*.py ); do
+      echo $t
+      python $t
+    done
+else
+    for t in $( ls test*.py ); do
+      echo $t
+      python $t -s
+    done
+fi
